fc99ea8f900f1417c0a26faa64702fa6334ea66a
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-10 12:25:00 +0300

    Monitoring directory example

--- src/main/java/com/colak/springbatchtutorial/refundbatch/scheduledtask/RefundScheduledTask.java
+++ src/main/java/com/colak/springbatchtutorial/refundbatch/scheduledtask/RefundScheduledTask.java
@@ -4,6 +4,7 @@ import com.colak.springbatchtutorial.refundbatch.service.RefundService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecutionException;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,8 @@ import java.nio.file.WatchService;
 import java.util.concurrent.TimeUnit;
 
 @Component
+@EnableScheduling
+
 @RequiredArgsConstructor
 @Slf4j
 public class RefundScheduledTask {
